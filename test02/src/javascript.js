eec3055a5c3e33ea8216d76ea99ac6938997af84
𝓡𝓐𝓝𝓙𝓔𝓔𝓣 𝓓𝓐𝓚𝓢𝓗 <164492985+ranjeetdakshvanshi@users.noreply.github.com>
2025-01-27 11:26:36 +0000

    TEST02

---
+++ test02/src/javascript.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".pic").forEach(function (pic) {
+    pic.addEventListener("click", function () {
+      document.querySelectorAll(".pic").forEach(function (pic) {
+        pic.classList.toggle("away"); // Toggle 'away' class on all elements with 'pic' class
+      });
+      this.classList.remove("away"); // Remove 'away' class from the clicked element
+      this.classList.toggle("active"); // Toggle 'active' class on the clicked element
+    });
+  });
+  
